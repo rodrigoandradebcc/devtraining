4a27cba682ea9386773e40dce78520083f4f1e68
rcandrade <rcandrade@rdm504017.local>
2022-12-19 00:22:29 -0300

    first commit

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+
+@Controller('courses')
+export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
+  @Get()
+  findAll() {
+    return this.coursesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.coursesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body('name') createCourseDto: CreateCourseDto) {
+    return this.coursesService.create(createCourseDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.coursesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string, @Body() body) {
+    return this.coursesService.remove(id);
+  }
+}
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,55 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Course } from './entities/Course.entity';
+
+@Injectable()
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do framework NestJS',
+      description: 'Fundamentos do framework NestJS',
+      tags: ['node.js', 'nestjs', 'javascript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    const course = this.courses.find(
+      (course: Course) => course.id === Number(id),
+    );
+
+    if (!course) {
+      throw new HttpException(
+        `Course id ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return course;
+  }
+
+  create(createCourseDto: any) {
+    return this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    if (indexCourse >= 0) {
+      this.courses.splice(indexCourse, 1);
+    }
+  }
+}
